63930095babe51cd260b79942a56585f2502a81c
0xTopoi <IamUser2021@protonmail.com>
2023-04-01 00:20:03 +0100

    first steps in defining voucher bootstraping

--- dos/src/lib.rs
+++ dos/src/lib.rs
@@ -1,5 +1,13 @@
 //! Degrees of Separation (DOS)
-//! 
+//!
 //! This is a simple implementation of the Degrees of Separation algorithm
-use plonky2;
+use plonky2::{
+    self, field::goldilocks_field::GoldilocksField, plonk::config::PoseidonGoldilocksConfig,
+};
 
+mod voucher;
+
+pub(crate) const D: usize = 2;
+pub(crate) type F = GoldilocksField;
+pub(crate) type C = PoseidonGoldilocksConfig;
+pub(crate) type GoldilocksAddress = [F; 5];
