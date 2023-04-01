e49affcc99cc0f16a26f7e5ed716c469c81ea1f5
0xTopoi <IamUser2021@protonmail.com>
2023-04-01 22:10:50 +0100

    add signature builder verification

--- dos/src/circuit_builder.rs
+++ dos/src/circuit_builder.rs
@@ -1,19 +1,17 @@
-use plonky2::{plonk::circuit_builder::CircuitBuilder, iop::target::Target};
-use simple_crypto::{
-    PUBLIC_KEY_LENGTH,
-    PRIVATE_KEY_LENGTH,
-    DIGEST_LENGTH,
-    F, C, D};
+use plonky2::{iop::target::Target, plonk::circuit_builder::CircuitBuilder};
+use simple_crypto::{C, D, DIGEST_LENGTH, F, PRIVATE_KEY_LENGTH, PUBLIC_KEY_LENGTH};
 
 pub struct VoucherTargets {
     pub(crate) origin_targets: Vec<Target>,
     pub(crate) locus_targets: Vec<Target>,
     pub(crate) degree_target: Target,
+    pub(crate) private_key_targts: Vec<Target>,
+    pub(crate) topic_pulic_key_targets: Vec<Target>,
+    pub(crate) should_be_public_key_origin_targets: Vec<Target>,
+    pub(crate) should_be_signature_targets: Vec<Target>,
 }
 
-pub(crate) fn make_origin_voucher_circuit(
-    builder: &mut CircuitBuilder<F, D>,
-) -> VoucherTargets {
+pub(crate) fn make_origin_voucher_circuit(builder: &mut CircuitBuilder<F, D>) -> VoucherTargets {
     // allocate targets for the public inputs
     let origin_targets = builder.add_virtual_targets(PUBLIC_KEY_LENGTH);
     let locus_targets = builder.add_virtual_targets(PUBLIC_KEY_LENGTH);
@@ -26,7 +24,7 @@ pub(crate) fn make_origin_voucher_circuit(
     // allocate remaining internal targets
     let private_key_targets = builder.add_virtual_targets(PRIVATE_KEY_LENGTH);
     let topic_public_key_targets = builder.add_virtual_targets(DIGEST_LENGTH);
-    
+
     // topic for public key is [F::ZERO; 4]
     for i in 0..DIGEST_LENGTH {
         builder.connect(topic_public_key_targets[i], builder.zero());
@@ -41,13 +39,29 @@ pub(crate) fn make_origin_voucher_circuit(
     builder.connect(degree_target, builder.zero());
 
     // the prover must know the private key, so it must hash to public key
-    let should_be_public_key_origin = 
-        builder.hash_n_to_hash_no_pad::<PoseidonHash>(
-            [private_key_targets, topic_public_key_targets].concat());
+    let should_be_public_key_origin_targets = builder.hash_n_to_hash_no_pad::<PoseidonHash>(
+        [private_key_targets, topic_public_key_targets].concat(),
+    );
     for i in 0..PUBLIC_KEY_LENGTH {
-        builder.connect(origin_targets[i], should_be_public_key_origin[i]);
+        builder.connect(origin_targets[i], should_be_public_key_origin_targets[i]);
     }
 
-    // the prover must sign the locus correctly, so they should hash 
-}
+    // the prover must sign the locus correctly, so they should hash the public key
+    // (origin) with the message (locuss)
+    let should_be_signature_targets =
+        builder.hash_n_to_hash_no_pad::<PoseidonHash>([origin_targets, locus_targets].concat());
+    for i in 0..DIGEST_LENGTH {
+        builder.connect(signature_targets[i], should_be_signature_targets[i]);
+    }
 
+    VoucherTargets {
+        origin_targets,
+        locus_targets,
+        signature_targets,
+        degree_target,
+        private_key_targets,
+        topic_public_key_targets,
+        should_be_public_key_origin_targets,
+        should_be_signature_targets,
+    }
+}
