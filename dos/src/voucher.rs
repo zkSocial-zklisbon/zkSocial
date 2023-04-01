b0d34a91bbbe09ca2aa80383df97ee4b392d2d51
0xTopoi <IamUser2021@protonmail.com>
2023-04-01 10:48:39 +0100

    resolve conflicts

--- dos/src/voucher.rs
+++ dos/src/voucher.rs
@@ -1,7 +1,22 @@
-use ed25519_proofs::{PublicKey, Signature, MessageDigest};
+use plonky2::{field::types::Field, plonk::circuit_data::CircuitConfig};
+
+use crate::{GoldilocksAddress, D, F};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+
+use ed25519_proofs::{MessageDigest, PublicKey, Signature};
 
 pub struct Voucher {
     pub(crate) origin: PublicKey,
     pub(crate) locus: PublicKey,
+}
 
+pub struct VoucherVerifier {}
+
+impl Voucher {
+    pub fn bootstrap(origin: PublicKey, locus: PublicKey, signature: &[u8], expiry: F) -> Self {
+        let config = CircuitConfig::standard_recursion_zk_config();
+        let mut circuit_builder = CircuitBuilder::<F, D>::new(config);
+
+        Self { origin, locus }
+    }
 }
